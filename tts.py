be744f7857e057f457f16a252d6bc56de3d0e525
FreydHub <egorkorshunov04@gmail.com>
2023-11-13 12:31:57 +0300

    Initial commit

---
+++ tts.py
@@ -0,0 +1,69 @@
+import torch
+import os
+import sounddevice as sd
+import time
+
+language = 'ru'
+model_id = 'ru_v3'
+sample_rate = 48000 # 48000
+speaker = 'kseniya' # aidar, baya, kseniya, xenia, random
+put_accent = True
+put_yo = True
+device = torch.device('cpu') # cpu или gpu
+text = "Привет"
+
+local_file = 'model.pt'
+
+model, _ = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                          model='silero_tts',
+                          language=language,
+                          speaker=model_id)
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
+                                   local_file)
+
+model.to(device)
+
+
+# воспроизводим
+def va_speak(what: str):
+    audio = model.apply_tts(text=what+"..",
+                            speaker=speaker,
+                            sample_rate=sample_rate,
+                            put_accent=put_accent,
+                            put_yo=put_yo)
+
+    sd.play(audio, sample_rate * 1.05)
+    time.sleep((len(audio) / sample_rate) + 0.5)
+    sd.stop()
+
+# sd.play(audio, sample_rate)
+# time.sleep(len(audio) / sample_rate)
+# sd.stop()
+
+'''
+import torch
+import zipfile
+import torchaudio
+from glob import glob
+
+device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
+model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                       model='silero_stt',
+                                       language='en', # also available 'de', 'es'
+                                       device=device)
+(read_batch, split_into_batches,
+ read_audio, prepare_model_input) = utils  # see function signature for details
+
+# download a single file in any format compatible with TorchAudio
+torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav',
+                               dst ='speech_orig.wav', progress=True)
+test_files = glob('speech_orig.wav')
+batches = split_into_batches(test_files, batch_size=10)
+input = prepare_model_input(read_batch(batches[0]),
+                            device=device)
+
+output = model(input)
+for example in output:
+    print(decoder(example.cpu()))
+    '''
